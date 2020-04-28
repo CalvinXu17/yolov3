faada284bdf8924fa949f635b16ca3f6b240d181
calvinxu17 <1084240577@qq.com>
2020-04-28 22:14:59 +0800

    first push

---
+++ tmp.py
@@ -0,0 +1,17 @@
+
+
+import cv2
+
+from PIL import Image
+
+
+
+img = cv2.imread('data/samples/bus.jpg')
+
+print(img.shape)
+
+img = Image.open('data/samples/bus.jpg')
+
+import numpy as np
+
+print(np.array(img).shape)
